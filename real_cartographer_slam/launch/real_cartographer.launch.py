2f81b861f7de78d4c706293b30ff938ef40deb89
Gokhulraj6200 <gokhulraj6200@gmail.com>
2024-08-08 17:13:05 +0000

    Created cartographer pkg for real robot and generated the map of the cafeteria

---
+++ real_cartographer_slam/launch/real_cartographer.launch.py
@@ -0,0 +1,56 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import TimerAction
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    cartographer_config_dir = os.path.join(get_package_share_directory('real_cartographer_slam'), 'config')
+    configuration_basename = 'real_cartographer.lua'
+
+    return LaunchDescription([
+        
+        Node(
+            package='cartographer_ros', 
+            executable='cartographer_node', 
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': False}],
+            arguments=['-configuration_directory', cartographer_config_dir,
+                       '-configuration_basename', configuration_basename],
+            remappings=[('/scan','/cleaner_2/scan'),
+                        ('/odom','/cleaner_2/odom'),
+                        ('/tf','/cleaner_2/tf')],
+        ),
+            
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_occupancy_grid_node',
+            output='screen',
+            name='occupancy_grid_node',
+            parameters=[{'use_sim_time': False}],
+            arguments=['-resolution', '0.05', '-publish_period_sec', '1.0'],
+            remappings=[('/scan','/cleaner_2/scan'),
+                        ('/odom','/cleaner_2/odom'),
+                        ('/tf','/cleaner_2/tf')],
+        ),
+
+        TimerAction(
+                period=2.0,
+                actions=[
+                    Node(
+                        package="tf2_ros",
+                        executable="static_transform_publisher",
+                        name="static_transform_publisher",
+                        namespace="",
+                        output="screen",
+                        arguments=[
+                            "0.075", "0", "0.150", "0", "0", "0", "base_footprint", "cleaner_2/laser_sensor_link"
+                        ]
+                    )
+                ]
+        )
+        
+    ]) 
