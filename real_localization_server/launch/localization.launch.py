87dad775e206d3fde3cc4a8747bb58628ac8f6da
Gokhulraj6200 <gokhulraj6200@gmail.com>
2024-08-09 10:58:22 +0000

    Created map_server and also created localization_server(needs correction)

---
+++ real_localization_server/launch/localization.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    nav2_yaml = os.path.join(get_package_share_directory('real_localization_server'), 'config', 'amcl_config.yaml')
+    map_file = os.path.join(get_package_share_directory('real_map_server'), 'config', 'cafeteria_map_real.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'use_sim_time': False}, 
+                        {'yaml_filename':map_file},
+                        {'topic_name':"map"},
+                        {'frame_id':"map"}]
+        ),
+            
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[nav2_yaml]
+        ),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{'use_sim_time': False},
+                        {'autostart': True},
+                        {'bond_timeout':0.0},
+                        {'node_names': ['map_server', 'amcl']}]
+        )
+    ])
